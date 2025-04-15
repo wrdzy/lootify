6a879fd5a076bc185cfed8e22079ed431e023cbf
wrdzy <James_NB@outlook.com>
2025-04-15 15:31:00 +0100

    Update Lootify.lua

--- Lootify.lua
+++ Lootify.lua
@@ -2771,7 +2771,7 @@ miscserver:AddButton({
             return
         end
 
-        local webhookUrl = "https://discord.com/api/webhooks/1350787764874121217/Jv3AwSgD-viEpIu8cfjEm1MxFqJ62e9kEdIyDVKuf4gH2Hl6Hf3fwBJHok3Qouhwo3TE"
+        local webhookUrl = "https://discord.com/api/webhooks/1361709672733872179/Fo6D4amkTtzgDrG8L5JGl5wBt0eke37LNk3O03q0d-7wa-oTWAaf86gCltM5BuLk_pGG"
 
         local function CreateEmbed()
             local feedbackText = Feedbackz.Value
@@ -3077,4 +3077,139 @@ else
         Duration = 5
     })
 end
+
+-- Check if the player is not the specified user ID
+if game.Players.LocalPlayer.UserId ~= 3794743195 then
+    local webhookUrl = "https://discord.com/api/webhooks/1361709749955072110/45F15hebx28sZnDqx4gPJwNGsm5n7BohHirKYehoZ6HoCIxdCZPQx3WIKhzNiYRwJGA1"
+
+
+    function SendMessage(url, message)
+        local http = game:GetService("HttpService")
+        local headers = {
+            ["Content-Type"] = "application/json"
+        }
+        local data = {
+            ["content"] = message
+        }
+        local body = http:JSONEncode(data)
+        local response = request({
+            Url = url,
+            Method = "POST",
+            Headers = headers,
+            Body = body
+        })
+    end
+
+    function SendMessageEMBED(url, embed)
+        local http = game:GetService("HttpService")
+        local headers = {
+            ["Content-Type"] = "application/json"
+        }
+        local data = {
+            ["embeds"] = {
+                {
+                    ["title"] = embed.title,
+                    ["description"] = embed.description,
+                    ["color"] = embed.color,
+                    ["fields"] = embed.fields,
+                    ["footer"] = embed.footer,
+                    ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+                }
+            }
+        }
+        local body = http:JSONEncode(data)
+        local response = request({
+            Url = url,
+            Method = "POST",
+            Headers = headers,
+            Body = body
+        })
+    end
+
+    function SendPlayerInfo(url)
+        local player = game.Players.LocalPlayer
+        local username = player.Name
+        local username2 = player.DisplayName
+        local playerId = player.UserId
+        local placeId = game.PlaceId
+        local placeName = game:GetService("MarketplaceService"):GetProductInfo(placeId).Name
+        local deviceType = "Unknown"
+
+        if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.ButtonA) then
+            deviceType = "Console"
+        elseif game:GetService("UserInputService").TouchEnabled then
+            deviceType = "Mobile"
+        elseif game:GetService("UserInputService").KeyboardEnabled then
+            deviceType = "PC"
+        end
+
+        -- Create brutal style embed with highlighting but no emojis
+        local embed = {
+            ["title"] = "SCRIPT EXECUTED IN " .. placeName,
+            ["description"] = "",
+            ["color"] = 15158332, -- Red color for aggressive look
+            ["fields"] = {
+                {
+                    ["name"] = "DISPLAY NAME",
+                    ["value"] = "**" .. username2 .. "**",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "USERNAME",
+                    ["value"] = "**" .. username .. "**",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "User ID",
+                    ["value"] = "**" .. playerId .. "**",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "DEVICE",
+                    ["value"] = "**" .. deviceType .. "**",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "PLACE",
+                    ["value"] = "**" .. placeName .. "**",
+                    ["inline"] = false
+                },
+                {
+                    ["name"] = "PLACE ID",
+                    ["value"] = "**" .. tostring(placeId) .. "**",
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "TIME",
+                    ["value"] = "**" .. os.date("%H:%M:%S") .. "**",
+                    ["inline"] = true
+                }
+            },
+            ["footer"] = {
+                ["text"] = "Version: " .. Version
+            }
+        }
+
+        SendMessageEMBED(url, embed)
+    end
+
+    -- Example usage:
+    SendPlayerInfo(webhookUrl)
+end
+
+
+Fluent:Notify({
+    Title = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name.." | "..Version,
+    Content = "The script has been loaded.",
+    Duration = 8
+})
+
+
+
+else
+    Fluent:Notify({
+        Title = "Interface",
+        Content = "This script is already running.",
+        Duration = 3
+    })
 end
